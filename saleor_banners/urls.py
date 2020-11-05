8c88dc8aa684b8f9c9bdcd20412e9c77ac4591da
ryan28561 <ryan@lightningkite.com>
2020-11-05 12:01:13 -0700

    allow no trailing slash

--- saleor_banners/urls.py
+++ saleor_banners/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls import url
 from .dashboard_views import views as dashboard_views
 
 urlpatterns = [
-    url(r'^dashboard/banners/$',
+    url(r'^dashboard/banners/?$',
         dashboard_views.saleor_banner_list,
         name='saleor-banner-dashboard-list'),
-    url(r'^dashboard/banners/create/$',
+    url(r'^dashboard/banners/create/?$',
         dashboard_views.saleor_banner_create,
         name='saleor-banner-dashboard-create'),
-    url(r'^dashboard/banners/(?P<pk>[0-9]+)/$',
+    url(r'^dashboard/banners/(?P<pk>[0-9]+)/?$',
         dashboard_views.saleor_banner_details,
         name='saleor-banner-dashboard-detail'),
-    url(r'^dashboard/banners/(?P<pk>[0-9]+)/delete/$',
+    url(r'^dashboard/banners/(?P<pk>[0-9]+)/delete/?$',
         dashboard_views.saleor_banner_delete,
         name='saleor-banner-dashboard-delete'),
 ]
